9344bfe84eede32333c67bc0466e22d3290655ae
Tim <Tim@10.30.16.182>
2016-01-28 13:28:20 -0600

    completed the lab

---
+++ src/bothLabs/EarlGrey.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bothLabs;
+
+/**
+ *
+ * @author Tim
+ */
+public class EarlGrey extends Tea{
+
+    @Override
+    public void drink() {
+        System.out.println("That was a nice Earl Grey");
+    }
+    
+}
